164da0b9839e3d10a0317f177d36ba4b4d4f2c10
lao_wan <lao_wan@sina.cn>
2016-12-12 19:27:25 +0800

    20161212下班

--- article/models.py
+++ article/models.py
@@ -2,9 +2,16 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return self.tag_name
+        
 class Article(models.Model):
     title = models.CharField(max_length = 100)#博客题目
-    category = models.CharField(max_length = 50, blank = True)#博客标签
+    category = models.CharField(max_length = 50, blank = True)#博客分类 可为空
+    tag = models.ManyToManyField(Tag, blank=True)  # 博客标签 可为空
     date_time = models.DateTimeField(auto_now_add = True)#博客日期
     content = models.TextField(blank = True, null = True)#博客正文
 
@@ -13,4 +20,6 @@ class Article(models.Model):
 
     class Meta:   #按时间下降排序
         ordering = ['-date_time']
+
+
 
--- article/views.py
+++ article/views.py
@@ -1,20 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
+from article.models import Tag
 from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 def home(request):
     #return HttpResponse("Hello World, Django")
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list' : post_list})
-
+"""
 def detail(request, my_args):
     post = Article.objects.all()[int(my_args)]
     str = ("title = %s, category = %s, date_time = %s, content = %s"
         % (post.title, post.category, post.date_time, post.content))
     return HttpResponse(str)
     #return HttpResponse("You're looking at my_args %s." % my_args)
+"""
 
 def test(request):
     return render(request, 'test.html',{'current_time': datetime.now()})
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+        tags = post.tag.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post': post, 'tags': tags})
+
+def about_me(request) :
+    return render(request, 'aboutme.html')
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoseNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list':post_list, 'error':False})
+
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category__iexact = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request,'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = s)
+            if len(post_list) == 0 :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : True})
+            else :
+                return render(request,'archives.html', {'post_list' : post_list,
+                                                    'error' : False})
+    return redirect('/')
